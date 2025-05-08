e50ee93c36de94dfb8663394c7f734c8b6182d8e
wetooa <derpykidyt@gmail.com>
2025-05-08 21:07:34 +0800

    Added quick tester

--- src/index.ts
+++ src/index.ts
@@ -2,25 +2,59 @@ import { Interpreter } from "./modules/interpreter/interpreter";
 import { Tokenizer } from "./modules/lexer/tokenizer";
 import { Parser } from "./modules/parser/parser";
 
-export function run(input: string, isRepl = false) {
-  console.log(`You entered: ${input}`);
+export function run(input: string, isRepl = false, isDebug = false) {
+  if (isDebug) {
+    console.log("Debug mode is enabled.");
+    console.log(`You entered: ${input}`);
+  }
+
   const lexer = new Tokenizer(isRepl);
   const parser = new Parser(isRepl);
   const interpreter = new Interpreter(isRepl);
   let output = "";
 
-  try {
-    const tokens = lexer.tokenize(input);
-    console.log("Tokens:", tokens);
+  if (isDebug) {
+    try {
+      const tokens = lexer.tokenize(input);
+      console.log("Tokens:", tokens);
 
-    const ast = parser.parse(tokens);
-    console.log("AST:", ast);
+      const ast = parser.parse(tokens);
+      console.log("AST:", ast);
 
-    output = interpreter.interpret(ast);
-    console.log("Output:", output);
-  } catch (error) {
-    throw new Error(`Error: ${error}`);
+      output = interpreter.interpret(ast);
+      console.log("==== SUCCESS =====");
+      console.log("Output:\n", output);
+    } catch (error) {
+      console.log("==== FAIL =====");
+      throw new Error(`Error: ${error}`);
+    }
+  } else {
+    try {
+      const tokens = lexer.tokenize(input);
+      const ast = parser.parse(tokens);
+      output = interpreter.interpret(ast);
+
+      console.log("==== SUCCESS =====");
+      console.log("Output:\n", output);
+    } catch (error) {
+      console.log("==== FAIL =====");
+      throw new Error(`Error: ${error}`);
+    }
   }
 
   return output;
 }
+
+const input = `
+SUGOD 
+  MUGNA NUMERO x = 1
+  IPAKITA: x
+  MUGNA TINUOD y = "OO"
+  IPAKITA: x & y & "HELLO"
+  MUGNA NUMERO z = 2
+  x = x + z * (5 - 1)
+  IPAKITA: x
+KATAPUSAN
+`;
+
+run(input);
